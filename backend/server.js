e0127e7b04d06a968e558ba9141647f8bad88fab
tashini maleesha <tashini.maleesha@gmail.com>
2025-06-15 00:40:05 +0530

    init routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const db = require('./db');
+const billRoutes = require('./routes/billRoutes');
+const inventoryRoutes = require('./routes/inventory');
 
 app.use(cors());
 
@@ -10,6 +12,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Nandana Paint Shop Backend Running');
 });
+
+app.use('/api/bill', billRoutes);
+app.use('/api/inventory', inventoryRoutes);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(` Backend running on http://localhost:${PORT}`));
