18a53ef79086639e5722a2e222fc6d8eb5f13726
tashini maleesha <tashini.maleesha@gmail.com>
2025-08-25 16:34:04 +0530

    change electrone js run as root

--- electron/main.js
+++ electron/main.js
@@ -7,7 +7,7 @@ function createWindow() {
     height: 800,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: false
     }
   });
 
@@ -15,3 +15,9 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
