4f51a3177a4e782211ae61cabf49fd16d14bfaa5
Ranvinu Hewage <123268275+rhewage3@users.noreply.github.com>
2025-08-25 16:25:20 +0530

    libraries

---
+++ frontend/js/links.js
@@ -0,0 +1,6 @@
+// Import jQuery
+document.write('<script src="js/libs/jquery.min.js"><\/script>');
+// Import Bootstrap
+document.write('<script src="js/libs/bootstrap.bundle.min.js"><\/script>');
+// Import DataTables
+document.write('<script src="js/libs/datatables.min.js"><\/script>');
